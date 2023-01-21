211d6d9f531172d24c6b23662a53dd491ff73353
Jina <77096179+Jina-sw@users.noreply.github.com>
2023-01-20 19:20:49 -0800

    nav background color change

---
+++ src/MainProgram/Program.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './Program.css';
+import Main from '../Main'
+import Navi from '../Navi'
+
+function Program(){
+    return(
+        <div>
+            <Navi></Navi>
+            
+        </div>
+        
+    )
+}
+
+export default Program;
