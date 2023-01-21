7693f8efdc23f38218255b3b9dd0f3e784b5d2a8
Jay Seung Yeon Lee <79389951+LeeJay0126@users.noreply.github.com>
2023-01-20 21:36:23 -0800

    .

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import MainPage from './MainPage/MainPage'
+import MainPage from './MainPage/MainPage';
+
+
 function App() {
   return (
     <div>
