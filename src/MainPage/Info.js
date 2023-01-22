dbadd8a495b7804a9776b923e15b7bb830b9c407
Jina <77096179+Jina-sw@users.noreply.github.com>
2023-01-21 23:24:40 -0800

    info
    update

--- src/MainPage/Info.js
+++ src/MainPage/Info.js
@@ -1,12 +1,29 @@
 import react from 'react';
+import first from '../assets/first.png'
+
 
 
 function info() {
     return (
-        <div>
-            <div>How much did you spend last month?</div>
-            <div>If you don't know how much you spent, this is the right time to track your money</div>
-            <img src="../assets/first.jpg"></img>
+        <div className="infoBG">
+            <br/>
+            <div className="b">How much did you spend last month?</div>
+            <p className="intro">....If you don't know how much you spent, this is the right time to track your money</p>
+            <br></br>
+            <div className="content_container">
+            <p className="content_title">Here Is What We Offer For You</p>
+                <div className="drawer">
+                    <p className="content_title">Firstly,<br/> click on one of the buttons at the right top</p>
+                    <img src={first} width="80%"></img>
+                </div>
+
+                <div className="drawer">
+                    <p className="content_title"><br/> Next, sign in or newly sign up to start <span>EXpenditure</span></p>
+                    
+                </div>
+                
+            </div>
+            
         </div>
     )
 }
